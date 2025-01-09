7ac9f2a5c1821be92c5858c3882b249f94f8c591
dxhunggg <hung071103@gmail.com>
2025-01-10 03:36:48 +0700

    26

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,10 +1,21 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { routes } from "./routes/index.js";
 import HeaderComponent from "./components/HeaderComponent/HeaderComponent.jsx";
 import DefaultComponent from "./components/DefaultComponent/DefaultComponent.jsx";
-
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 const App = () => {
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
+  const fetchApi = async () => {
+    const apiUrl = import.meta.env.VITE_API_URL_BACKEND;    
+    const res = await axios.get(`${apiUrl}/product/get-all`);
+    return res.data;
+  };
+  const query = useQuery({ queryKey: ["todos"], queryFn: fetchApi });
   return (
     <div>
       <Router>
--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -4,9 +4,16 @@ import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
 import store from "./redux/store.js";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const queryClient = new QueryClient();
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
