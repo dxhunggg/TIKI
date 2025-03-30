cbfac7d859c055593a78d0c4cdb84c77ee9a89a4
dxhunggg <hung071103@gmail.com>
2025-03-30 16:23:29 +0700

    51

--- FRONTEND/src/pages/TypeProductPage/TypeProductPage.jsx
+++ FRONTEND/src/pages/TypeProductPage/TypeProductPage.jsx
@@ -7,28 +7,37 @@ import { useLocation } from "react-router-dom";
 import * as ProductService from "../../services/ProductService";
 import { useEffect } from "react";
 import Loading from "../../components/LoadingComponent/Loading";
-
+import { useSelector } from "react-redux";
+import { useDebounce } from "../../hooks/useDebounce";
 const TypeProductPage = () => {
+  const searchProduct = useSelector((state) => state?.product?.search);
+  const searchDebounce = useDebounce(searchProduct, 500);
   const { state } = useLocation();
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
-  const fetchProductType = async (type) => {
+  const [panigate, setPanigate] = useState({
+    page: 0,
+    limit: 10,
+    total: 1,
+  });
+  const fetchProductType = async (type, page, limit) => {
     setLoading(true);
-    const res = await ProductService.getProductType(type);
-    if (res?.status === "OK") {
+    const res = await ProductService.getProductType(type, page, limit);
+    if (res?.status == "OK") {
       setLoading(false);
       setProducts(res?.data);
+      setPanigate({ ...panigate, total: res?.totalPage });
     } else {
       setLoading(false);
     }
   };
   useEffect(() => {
     if (state) {
-      fetchProductType(state);
+      fetchProductType(state, panigate.page, panigate.limit);
     }
-  }, [state]);
-  const onChange = (page, pageSize) => {
-    console.log(page, pageSize);
+  }, [state, panigate.page, panigate.limit]);
+  const onChange = (current, pageSize) => {
+    setPanigate({ ...panigate, page: current - 1, limit: pageSize });
   };
   return (
     <Loading isLoading={loading}>
@@ -39,11 +48,12 @@ const TypeProductPage = () => {
           height: "calc(100vh - 64px)",
         }}
       >
-        <div style={{ width: "1270px", margin: "0 auto" }}>
+        <div style={{ width: "1270px", margin: "0 auto", height: "100%" }}>
           <Row
             style={{
               flexWrap: "nowrap",
               paddingTop: "10px",
+              height: "calc(100% - 20px)",
             }}
           >
             <WrapperNavBar span={4}>
@@ -58,27 +68,39 @@ const TypeProductPage = () => {
               }}
             >
               <WrapperProducts>
-                {products?.map((product) => {
-                  return (
-                    <CardComponent
-                      key={product._id}
-                      countInStock={product.countInStock}
-                      description={product.description}
-                      image={product.image}
-                      name={product.name}
-                      price={product.price}
-                      rating={product.rating}
-                      type={product.type}
-                      selled={product.selled}
-                      discount={product.discount}
-                      id={product._id}
-                    />
-                  );
-                })}
+                {products
+                  ?.filter((pro) => {
+                    if (searchDebounce === "") {
+                      return pro;
+                    } else if (
+                      pro?.name
+                        ?.toLowerCase()
+                        ?.includes(searchDebounce?.toLowerCase())
+                    ) {
+                      return pro;
+                    }
+                  })
+                  ?.map((product) => {
+                    return (
+                      <CardComponent
+                        key={product._id}
+                        countInStock={product.countInStock}
+                        description={product.description}
+                        image={product.image}
+                        name={product.name}
+                        price={product.price}
+                        rating={product.rating}
+                        type={product.type}
+                        selled={product.selled}
+                        discount={product.discount}
+                        id={product._id}
+                      />
+                    );
+                  })}
               </WrapperProducts>
               <Pagination
-                defaultCurrent={2}
-                total={100}
+                defaultCurrent={panigate.page + 1}
+                total={panigate?.total}
                 onChange={onChange}
                 style={{ textAlign: "center", marginTop: "10px" }}
               />
