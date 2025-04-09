46d24a4866c064e5527a3ad9339e4a39d726fa3a
dxhunggg <hung071103@gmail.com>
2025-04-10 02:44:27 +0700

    xoa may file thua

--- OrderPage.jsx
+++
@@ -1,6 +0,0 @@
-const calculateShippingFee = () => {
-    const totalAmount = calculateTotal();
-    if (totalAmount >= 500000) return 0;
-    if (totalAmount >= 200000) return 10000;
-    return 20000;
-}; 
