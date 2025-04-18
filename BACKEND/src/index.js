a61198a36f86144786c55592bdd1230ffe8c0e11
dxhunggg <hung071103@gmail.com>
2025-04-19 02:44:56 +0700

    Your commit message

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -9,7 +9,24 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://bachhoaxanh-nhjxtxp2q-xuan-hung-djinhhs-projects.vercel.app",
+  
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
